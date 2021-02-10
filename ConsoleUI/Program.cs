e8da6444957b349ec21ace545a54e1f77e24305e
Erdem Demircioglu <64333517+erdem6161@users.noreply.github.com>
2021-02-10 22:48:05 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal);
+            foreach (var category in categoryManager.GetAll()) ;
+
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
